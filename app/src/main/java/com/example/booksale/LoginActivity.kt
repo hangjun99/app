2c61c8649894e3ecfb3d984f1d58d2bb733017cb
Park-yearin <rabbit2122@daum.net>
2023-12-03 20:47:01 +0900

    login

--- app/src/main/java/com/example/booksale/LoginActivity.kt
+++ app/src/main/java/com/example/booksale/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.booksale
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,6 +12,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class LoginActivity : AppCompatActivity(){
     private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainpage_activity)
@@ -19,40 +22,37 @@ class LoginActivity : AppCompatActivity(){
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         // 하단 버튼 눌러 화면 전환
-        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNavigation:BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.fragment_home -> {
                     val intent = Intent(this@LoginActivity, LoginActivity::class.java)
                     startActivity(intent)
-                    // Respond to navigation item 1 click
+                    finish()
                 }
-                R.id.fragment_booksale -> {
-                    bottomNavigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv2)
-                    bottomNavigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv2)
 
-                    // Respond to navigation item 2 click
+                R.id.fragment_booksale -> {
+
                 }
+
                 R.id.search -> {
-                    bottomNavigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv2)
-                    bottomNavigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv2)
-                    // Respond to navigation item 3 click
+
                 }
+
                 R.id.fragment_chatList->{
                     val intent = Intent(this@LoginActivity, ChatlistActivity::class.java)
                     startActivity(intent)
+                    finish()
                 }
-                else -> {
-                    bottomNavigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv1)
-                    bottomNavigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv1)
-                    MainActivity()
+                R.id.fragment_myPage ->{
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java )
+                    startActivity(intent)
+                    finish()
                 }
             }
             true
         }
-        bottomNavigation.selectedItemId = R.id.fragment_home
+        bottomNavigation.selectedItemId = R.id.fragment_home //초기값 세팅
     }
 
-
-
-    }
+}

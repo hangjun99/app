1e8347751a00b83d0b0548c0e4e5af4a80c55d6a
Park-yearin <rabbit2122@daum.net>
2023-12-05 22:01:47 +0900

    login

--- app/src/main/java/com/example/booksale/SearchActivity.kt
+++ app/src/main/java/com/example/booksale/SearchActivity.kt
@@ -19,42 +19,5 @@ class SearchActivity : AppCompatActivity() {
         searchView = findViewById(R.id.search)
 
 
-        // Set up SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                searchMovie(query!!)
-                // Handle the search query submission if needed
-                return false
-            }
-
-            private fun searchMovie(query: String) {
-
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                filterBooks(newText)
-                return true
-            }
-        })
-
-
     }
-    private fun filterBooks(query: String?) {
-        val url = "http://13.209.64.52/book_insert.php?searchQuery=$query"
-
-        val request = JsonObjectRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                // Handle the JSON response here
-                // Parse the JSON response and update your RecyclerView accordingly
-            },
-            { error ->
-                // Handle errors
-                Log.e("SearchActivity", "Error: $error")
-            })
-
-        // Add the request to the Volley request queue
-        Volley.newRequestQueue(this).add(request)
-    }
-
 }

401fa828c097a80ad263ac95c8c6d9dc3c27542a
Park-yearin <rabbit2122@daum.net>
2023-12-08 09:54:16 +0900

    login

--- app/src/main/java/com/example/booksale/ChatlistActivity.kt
+++ app/src/main/java/com/example/booksale/ChatlistActivity.kt
@@ -1,14 +1,116 @@
 package com.example.booksale
 
+
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.Volley
+import io.socket.client.IO
+import io.socket.client.Socket
+import org.json.JSONException
+import org.json.JSONObject
+import java.net.URISyntaxException
 
 class ChatlistActivity : AppCompatActivity() {
+    lateinit var sendButton: Button
+    lateinit var messageEditText: EditText
+    lateinit var nickname: String
+    lateinit var chatroom: String
+
+    private lateinit var socket: Socket
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
 
+        sendButton = findViewById<Button>(R.id.sendButton)
+        messageEditText = findViewById<EditText>(R.id.messageEditText)
+        //nickname chatroom 입력하세요
+        //nickname = ~~~~~
+        //chatroom = ~~~~~
+        nickname = "dy"
+        chatroom = "1-2"
 
+        try {
+            socket = IO.socket("http://13.209.64.52:3000")
+            socket.connect()
+            Log.d("Connected", "OK")
+        } catch (e: URISyntaxException) {
+            Log.d("ERR", e.toString())
+        }
+
+        socket.on(Socket.EVENT_CONNECT) {
+            socket.emit("enter_room", chatroom)
+        }
+
+        socket.on("new_message") { msg ->
+            val message = msg[0].toString()
+            messageEditText.setText(message)
+            // 메시지 수신 시 하고 창에 받은 메세지 보여주는 코드 추가해주세요
+            displayReceivedMessage(message)
+
+        }
+
+        sendButton.setOnClickListener {
+            val message = messageEditText.text.toString()
+            socket.emit("new_message", message, chatroom)
+            messageEditText.text.clear()
+            //자신의 nickname과 message로 보낸 메세지 보여주는 코드 추가해주세요
+
+            // 자신이 보낸 메세지를 UI에 표시
+            displaySentMessage(nickname, message)
+
+            // 메시지 입력란 초기화
+            messageEditText.text.clear()
+
+            val responseListener: Response.Listener<String?> =
+                Response.Listener<String?> { response ->
+                    try {
+                        val jsonObject = JSONObject(response)
+                        val success = jsonObject.getBoolean("success")
+                        if (success) {
+                            Toast.makeText(applicationContext, "메시지 전송", Toast.LENGTH_SHORT).show()
+                            Log.d("ChatlistActivity", "메시지 전송성공")
+                        } else {
+                            Toast.makeText(applicationContext, "실패", Toast.LENGTH_SHORT).show()
+                            return@Listener
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                        Toast.makeText(applicationContext, "예외 1", Toast.LENGTH_SHORT).show()
+                        return@Listener
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            val msgRequestActivity = MsgRequestActivity("1-2","1", "2", message, responseListener)
+            val queue = Volley.newRequestQueue(applicationContext)
+            queue.add(msgRequestActivity)
+
+        }
+
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        socket.disconnect()
+        socket.emit("left", chatroom)
+    }
+    // UI에 받은 메시지를 표시하는 함수
+    private fun displayReceivedMessage(message: String) {
+        // 받은 메시지를 화면에 표시하는 코드를 추가
+        val formattedMessage = "상대방: $message"
+        messageEditText.setText(formattedMessage)
+    }
+    // UI에 자신이 보낸 메시지를 표시하는 함수
+    private fun displaySentMessage(senderNickname: String, message: String) {
+        // 자신이 보낸 메시지를 화면에 표시하는 코드를 추가
+        val sentMessage = "$senderNickname: $message"
+        messageEditText.setText(sentMessage)
     }
 
 }
--- app/src/main/java/com/example/booksale/LoginActivity.kt
+++ app/src/main/java/com/example/booksale/LoginActivity.kt
@@ -1,40 +1,19 @@
 package com.example.booksale
 
-import android.content.ClipData
+
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.Volley
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import org.json.JSONArray
-import org.json.JSONException
 
 class LoginActivity : AppCompatActivity(){
 
-    var items = ArrayList<ClipData.Item>()
-    var adapter: ItemAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainpage_activity)
 
-        // 등록된 상품을 홈 화면에 보여주기
-        var recyclerView:RecyclerView= findViewById(R.id.list)
-        //adapter = ItemAdapter(this, items)
-        recyclerView.setAdapter(adapter)
-
-        //리사이클러뷰의 레이아웃 매니저 설정
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerView.setLayoutManager(layoutManager)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigation.setOnItemSelectedListener { item ->
@@ -59,7 +38,7 @@ class LoginActivity : AppCompatActivity(){
                     bottomNavigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv2)
                     bottomNavigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv2)
                     Toast.makeText(applicationContext, "검색", Toast.LENGTH_SHORT).show()
-                    val intent= Intent(this@LoginActivity, SearchActivity::class.java)
+                    val intent= Intent(this@LoginActivity, ItemActivity::class.java)
                     startActivity(intent)
 
                 }
@@ -67,7 +46,7 @@ class LoginActivity : AppCompatActivity(){
                     bottomNavigation.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv2)
                     bottomNavigation.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv2)
                     Toast.makeText(applicationContext, "챗", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, ChatlistActivity::class.java)
+                    val intent = Intent(this@LoginActivity, ChatlistActivity::class.java)
                     startActivity(intent)
                 }
                 else -> {
@@ -80,64 +59,5 @@ class LoginActivity : AppCompatActivity(){
         }
         bottomNavigation.selectedItemId = R.id.fragment_home
     }
-    fun clickLoad(view: View?) {
-
-        //서버의 php파일에 접속하여 (DB데이터들)결과 받기
-        //Volley+ 라이브러리 사용
-
-        //서버주소
-        val serverUrl = "http://13.209.64.52/book_insert.php"
-
-        //결과를 JsonArray 받을 것이므로..
-        //StringRequest가 아니라..
-        //JsonArrayRequest를 이용할 것임
-        val jsonArrayRequest =
-            JsonArrayRequest(Request.Method.POST, serverUrl, null, object :
-                Response.Listener<JSONArray?> {
-                //volley 라이브러리의 GET방식은 버튼 누를때마다 새로운 갱신 데이터를 불러들이지 않음. 그래서 POST 방식 사용
-                fun onResponse(response: JSONArray) {
-                    Toast.makeText(this@LoginActivity, response.toString(), Toast.LENGTH_SHORT)
-                        .show()
-
-
-                    //파라미터로 응답받은 결과 JsonArray를 분석
-                    items.clear()
-                    adapter?.notifyDataSetChanged()
-                    try {
-                        for (i in 0 until response.length()) {
-                            val jsonObject = response.getJSONObject(i)
-                            val BookName = jsonObject.getString("BookName")
-                            val Edit= jsonObject.getString("Edit")
-                            val Author = jsonObject.getString("Author")
-                            var Publisher= jsonObject.getString("Publisher")
-                            val HopePrice = jsonObject.getString("HopePrice")
-
-                            //items.add(
-                                ///* index = */ 0,
-                                /* element = */ //ClipData.Item(BookName, Edit, Author, Publisher, HopePrice) ) // 첫 번째 매개변수는 몇번째에 추가 될지, 제일 위에 오도록
-                            adapter?.notifyItemInserted(0)
-                        }
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                    }
-                }
-
-                override fun onResponse(response: JSONArray?) {
-                    TODO("Not yet implemented")
-                }
-            }, object : Response.ErrorListener {
-                override fun onErrorResponse(error: VolleyError?) {
-                    Toast.makeText(this@LoginActivity, "ERROR", Toast.LENGTH_SHORT).show()
-                }
-            })
-
-        //실제 요청 작업을 수행해주는 요청큐 객체 생성
-        val requestQueue: RequestQueue = Volley.newRequestQueue(this)
-
-        //요청큐에 요청 객체 생성
-        requestQueue.add(jsonArrayRequest)
-    }
-
-
 
 }
